d15eeabdde972212a332569e96027d5938b47553
omersaritoy <fltepior@gmail.com>
2026-02-06 18:16:42 +0300

    added validation and docker-compose.yml for access to project

--- backend/src/main/java/com/TalentWorld/backend/controller/AuthController.java
+++ backend/src/main/java/com/TalentWorld/backend/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.TalentWorld.backend.dto.request.SignInRequest;
 import com.TalentWorld.backend.dto.request.SignupRequest;
 import com.TalentWorld.backend.dto.response.AuthResponse;
 import com.TalentWorld.backend.service.impl.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +20,11 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping
-    public ResponseEntity<AuthResponse> signup(@RequestBody SignupRequest signupRequest) {
+    public ResponseEntity<AuthResponse> signup(@Valid @RequestBody SignupRequest signupRequest) {
         return ResponseEntity.ok(authService.signup(signupRequest));
     }
     @PostMapping("/signin")
-    public ResponseEntity<AuthResponse> signin(@RequestBody SignInRequest signinRequest) {
+    public ResponseEntity<AuthResponse> signin(@Valid @RequestBody SignInRequest signinRequest) {
         return ResponseEntity.ok(authService.singin(signinRequest));
     }
 
